240537889a8a1a9bf327cddac3299d4af859d715
erlanggaht <erlanggahidayat.md@gmail.com>
2023-05-06 11:40:07 +0700

    setheader

--- src/app/page.js
+++ src/app/page.js
@@ -54,6 +54,12 @@ export default function Page() {
     try {
       const response = await axios.get(`${localhost_port}/token`, {
         withCredentials: true,
+        headers : {
+          "Content-Type" : 'application/json',
+          'Access-Control-Allow-Origin' : '*',
+          'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',    
+      },
+      'Access-Control-Allow-Origin' : '*',
       });
       setToken(response.data.accesstoken);
       const decoded = jwtDecode(response.data.accesstoken);
@@ -78,6 +84,12 @@ export default function Page() {
       if (expired * 1000 < currentDate.getTime()) {
         const response = await axios.get(`${localhost_port}/token`, {
           withCredentials: true,
+          headers : {
+            "Content-Type" : 'application/json',
+            'Access-Control-Allow-Origin' : '*',
+            'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',    
+        },
+        'Access-Control-Allow-Origin' : '*',
         });
         config.headers.Authorization = `Bearer ${response.data.accesstoken}`;
         setToken(response.data.accesstoken);
