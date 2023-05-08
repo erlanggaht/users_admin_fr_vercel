901e8ef1734d898adf356267f52c30b6f61444a6
erlanggaht <erlanggahidayat.md@gmail.com>
2023-05-08 12:20:58 +0700

    okeh

--- src/app/login/validate.js
+++ src/app/login/validate.js
@@ -3,7 +3,7 @@ import * as Yup from 'yup'
 import isEmailValidator from 'validator/lib/isEmail';
 import axios from "axios";
 import { useState } from "react";
-import {useRouter} from "next/router";
+import {useRouter} from "next/navigation";
 
 export default function validateLogin() {
     const [loadingButton,setButtonLoading] = useState(false)
